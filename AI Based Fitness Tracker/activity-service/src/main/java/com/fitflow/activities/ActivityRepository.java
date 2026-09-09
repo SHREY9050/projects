cb4739b2a71cdf39aae7ca1684d6feef9a3dd792
SHREY9050 <116563424+SHREY9050@users.noreply.github.com>
2026-09-10 00:28:26 +0530

    Add Spring microservices application file: activity-service/src/main/java/com/fitflow/activities/ActivityRepository.java

---
+++ AI Based Fitness Tracker/activity-service/src/main/java/com/fitflow/activities/ActivityRepository.java
@@ -0,0 +1,2 @@
+package com.fitflow.activities;import java.time.Instant;import java.util.*;import org.springframework.data.mongodb.repository.MongoRepository;public interface ActivityRepository extends MongoRepository<Activity,String>{List<Activity> findByUserIdOrderByStartTimeDesc(String userId);List<Activity> findByUserIdAndStartTimeGreaterThanEqual(String userId,Instant start);}
+
