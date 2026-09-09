e0418005ccf33e325bfac21c550d5ee58193a423
SHREY9050 <116563424+SHREY9050@users.noreply.github.com>
2026-09-10 00:28:41 +0530

    Add Spring microservices application file: activity-service/src/main/java/com/fitflow/activities/Activity.java

---
+++ AI Based Fitness Tracker/activity-service/src/main/java/com/fitflow/activities/Activity.java
@@ -0,0 +1,4 @@
+package com.fitflow.activities;
+import java.time.*;import java.util.*;import org.springframework.data.annotation.Id;import org.springframework.data.mongodb.core.mapping.Document;
+@Document("activities") public class Activity{@Id public String id;public String userId;public String type;public int duration;public int caloriesBurned;public Integer steps;public Double distanceKm;public Integer averageHeartRate;public Double paceMinutesPerKm;public Instant startTime;public Map<String,Object> additionalMetrics=new HashMap<>();public Instant createdAt=Instant.now();}
+
