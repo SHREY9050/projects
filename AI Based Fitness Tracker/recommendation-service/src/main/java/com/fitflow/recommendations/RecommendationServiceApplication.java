f3a822d7ed1631943a8fd3234c037b374830208f
SHREY9050 <116563424+SHREY9050@users.noreply.github.com>
2026-09-10 00:28:42 +0530

    Add Spring microservices application file: recommendation-service/src/main/java/com/fitflow/recommendations/RecommendationServiceApplication.java

---
+++ AI Based Fitness Tracker/recommendation-service/src/main/java/com/fitflow/recommendations/RecommendationServiceApplication.java
@@ -0,0 +1,2 @@
+package com.fitflow.recommendations;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;@SpringBootApplication public class RecommendationServiceApplication{public static void main(String[] a){SpringApplication.run(RecommendationServiceApplication.class,a);}}
+
