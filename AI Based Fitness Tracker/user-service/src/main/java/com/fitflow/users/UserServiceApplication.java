68e475fece1f1da09480cf3f48478f602d9d1e4e
SHREY9050 <116563424+SHREY9050@users.noreply.github.com>
2026-09-10 00:28:13 +0530

    Add Spring microservices application file: user-service/src/main/java/com/fitflow/users/UserServiceApplication.java

---
+++ AI Based Fitness Tracker/user-service/src/main/java/com/fitflow/users/UserServiceApplication.java
@@ -0,0 +1,4 @@
+package com.fitflow.users;
+import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication public class UserServiceApplication { public static void main(String[] args){SpringApplication.run(UserServiceApplication.class,args);} }
+
