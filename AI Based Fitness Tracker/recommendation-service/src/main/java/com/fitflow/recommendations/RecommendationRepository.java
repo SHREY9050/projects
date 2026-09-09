3208f371d124b0475b3ce6e909174312b49a0bb2
SHREY9050 <116563424+SHREY9050@users.noreply.github.com>
2026-09-10 00:28:46 +0530

    Add Spring microservices application file: recommendation-service/src/main/java/com/fitflow/recommendations/RecommendationRepository.java

---
+++ AI Based Fitness Tracker/recommendation-service/src/main/java/com/fitflow/recommendations/RecommendationRepository.java
@@ -0,0 +1,2 @@
+package com.fitflow.recommendations;import java.util.*;import org.springframework.data.mongodb.repository.MongoRepository;public interface RecommendationRepository extends MongoRepository<Recommendation,String>{List<Recommendation> findByUserIdOrderByCreatedAtDesc(String userId);}
+
